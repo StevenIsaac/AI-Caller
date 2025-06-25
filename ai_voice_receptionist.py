4e17879d25c4835cf1ff4e79143f98c30aa6e372
Steven I <steven@Stevens-MacBook-Air.local>
2025-06-25 15:59:02 -0500

    bind to 0.0.0.0

--- ai_voice_receptionist.py
+++ ai_voice_receptionist.py
@@ -34,4 +34,6 @@ def voice():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
